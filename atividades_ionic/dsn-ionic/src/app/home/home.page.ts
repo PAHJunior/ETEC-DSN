e073b698097022aebb83107083615a2924d7f300
PAHJunior <pahjunior@outlook.com>
2019-08-01 00:28:35 -0300

    projetos ETEC Embu 2019 :alien:

---
+++ atividades_ionic/dsn-ionic/src/app/home/home.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(public alertController: AlertController) {}
+  
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'DSN - IONIC',
+      subHeader: 'IONIC',
+      message: 'This is ETEC MAN.',
+      buttons: ['OK']
+    });
+  
+    await alert.present();
+  }
+
+}
